b32f892b8a2e2481ce8e9987539dc0c6a6053778
Tim Hoiberg <tim.hoiberg@gmail.com>
2023-06-22 22:51:22 +0900

    Setup basic Rust HTTP server and load the Vite bundle
    It works with the exception of relative assets in Vite, as it's
    expecting them to be loaded on the same port as the Rust server, which
    it isn't. Probably not a right now, can be worked out later.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{response::Html, routing::get, Router};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "japanese-study-tracker=info".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new().route("/", get(root_handler));
+
+    let address = SocketAddr::from(([0, 0, 0, 0], 3000));
+
+    tracing::info!("listening on {}", address);
+
+    axum::Server::bind(&address)
+        .serve(app.into_make_service())
+        .await
+        .unwrap()
+}
+
+async fn root_handler() -> Html<&'static str> {
+    Html(include_str!("templates/index.html"))
 }
