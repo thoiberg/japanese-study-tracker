b981cb3b8560015a355c710a35ecef7fa0fa3f04
Tim Hoiberg <tim.hoiberg@gmail.com>
2023-06-23 23:03:02 +0900

    Fetch data from wanikani and forward it for the frontend

--- backend/src/api/mod.rs
+++ backend/src/api/mod.rs
@@ -1,2 +1,21 @@
+use axum::{http::StatusCode, Json};
+
 pub mod wanikani;
-// pub use wanikani;
+
+#[derive(serde::Serialize)]
+pub struct ErrorResponse {
+    message: String,
+}
+
+pub fn internal_error<E>(err: E) -> (StatusCode, Json<ErrorResponse>)
+where
+    E: Into<anyhow::Error>,
+{
+    let err = err.into();
+    (
+        StatusCode::INTERNAL_SERVER_ERROR,
+        Json(ErrorResponse {
+            message: err.to_string(),
+        }),
+    )
+}
