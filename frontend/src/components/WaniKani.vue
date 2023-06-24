0789f17c652575ac512fe11fc1b06a9fbc818765
Tim Hoiberg <tim.hoiberg@gmail.com>
2023-06-24 15:53:00 +0900

    Add basic error handling and tests

--- frontend/src/components/WaniKani.vue
+++ frontend/src/components/WaniKani.vue
@@ -1,8 +1,11 @@
 <template>
   <div v-if="waniKaniData">
-    <div>{{ waniKaniData.active_lesson_count }}</div>
-    <div>{{ waniKaniData.active_review_count }}</div>
-    <div>{{ new Date(waniKaniData.data_updated_at) }}</div>
+    <div>Current Lessons: {{ waniKaniData.active_lesson_count }}</div>
+    <div>Current Reviews: {{ waniKaniData.active_review_count }}</div>
+    <div>Data Fetched at: {{ new Date(waniKaniData.data_updated_at) }}</div>
+  </div>
+  <div v-else-if="error">
+    <p>{{ error.message }}</p>
   </div>
   <div v-else>Loading...</div>
 </template>
@@ -11,15 +14,29 @@
 import { onMounted, ref, type Ref } from 'vue'
 
 let waniKaniData: Ref<WaniKaniResponse | null> = ref(null)
+let error: Ref<BackendError | null> = ref(null)
 
 onMounted(async () => {
   const response = await fetch('/api/wanikani')
-  waniKaniData.value = await response.json()
+
+  try {
+    if (response.status < 400) {
+      waniKaniData.value = await response.json()
+    } else {
+      error.value = await response.json()
+    }
+  } catch (e) {
+    error.value = { message: 'Unexpected error occurred.' }
+  }
 })
 
 type WaniKaniResponse = {
   data_updated_at: string
   active_lesson_count: number
   active_review_count: number
+}
+
+type BackendError = {
+  message: string
 }
 </script>
---
+++ frontend/src/components/__tests__/WaniKani.spec.ts
@@ -0,0 +1,62 @@
+import { describe, it, vi, expect, afterEach } from 'vitest'
+import { mount, flushPromises } from '@vue/test-utils'
+
+import WaniKaniVue from '../WaniKani.vue'
+
+describe('WaniKani', () => {
+  const fetchMock = vi.fn()
+  global.fetch = fetchMock
+
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  it('displays a loading indicator', async () => {
+    const mockResponse = {
+      json: () => new Promise((resolve) => resolve({}))
+    }
+    fetchMock.mockResolvedValue(mockResponse)
+
+    const wrapper = mount(WaniKaniVue)
+
+    expect(wrapper.text()).toContain('Loading...')
+  })
+
+  describe('when the request succeeds', () => {
+    it('displays the information', async () => {
+      const data = {
+        data_updated_at: '2023-06-24T06:00:00Z',
+        active_lesson_count: 66,
+        active_review_count: 15
+      }
+      const mockResponse = {
+        status: 200,
+        json: () => new Promise((resolve) => resolve(data))
+      }
+      fetchMock.mockResolvedValue(mockResponse)
+
+      const wrapper = mount(WaniKaniVue)
+      await flushPromises()
+
+      expect(wrapper.text()).toContain('66')
+    })
+  })
+
+  describe('when the request fails', () => {
+    it('displays an error message', async () => {
+      const data = {
+        message: 'uh oh'
+      }
+      const mockResponse = {
+        status: 500,
+        json: () => new Promise((resolve) => resolve(data))
+      }
+      fetchMock.mockResolvedValue(mockResponse)
+
+      const wrapper = mount(WaniKaniVue)
+      await flushPromises()
+
+      expect(wrapper.text()).toContain('uh oh')
+    })
+  })
+})
