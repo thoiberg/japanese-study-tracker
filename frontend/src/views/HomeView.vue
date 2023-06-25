f7875d1b94ea783f6510c12f33208381054d13ba
Tim Hoiberg <tim.hoiberg@gmail.com>
2023-06-25 18:48:29 +0900

    Divide page into quarters and add place holders

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,7 +3,33 @@ import WaniKani from '@/components/WaniKani.vue'
 </script>
 
 <template>
-  <main>
-    <WaniKani />
+  <main class="grid-container">
+    <div class="item-container">
+      <WaniKani />
+    </div>
+    <div class="item-container">
+      <p>Bunpro</p>
+    </div>
+    <div class="item-container">
+      <p>Satori</p>
+    </div>
+    <div class="item-container">
+      <p>Anki</p>
+    </div>
   </main>
 </template>
+
+<style scoped>
+.grid-container {
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+
+.item-container {
+  display: grid;
+  grid-template-columns: subgrid;
+  align-items: center;
+  justify-items: center;
+}
+</style>
