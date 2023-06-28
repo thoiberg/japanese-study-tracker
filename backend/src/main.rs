9a662ca062b2141083a9e0721fe19deec7680ad7
Tim Hoiberg <tim.hoiberg@gmail.com>
2023-06-28 21:37:51 +0900

    Re-org server structure
    To make deploying the app easier (sticking everything into one
    container) I want the backend to look for and serve assets in a similar
    structure to what vite build will output. This way I can just copy over
    the FE assets from the frontend container when building the production
    image.
    
    It's a bit janky, but workable for now.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,7 @@
-use std::net::SocketAddr;
+use std::{fs, net::SocketAddr};
 
-use axum::{response::Html, routing::get, Router};
+use axum::{http::StatusCode, response::Html, routing::get, Router};
+use tower_http::services::ServeDir;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 pub mod api;
@@ -17,7 +18,9 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
+    // TODO add nest service to serve dist/assets directory
     let app = Router::new()
+        .merge(Router::new().nest_service("/assets", ServeDir::new("dist/assets")))
         .route("/", get(root_handler))
         .route("/api/wanikani", get(wanikani_handler));
 
@@ -31,6 +34,19 @@ async fn main() {
         .unwrap()
 }
 
-async fn root_handler() -> Html<&'static str> {
-    Html(include_str!("templates/index.html"))
+async fn root_handler() -> Result<Html<String>, (StatusCode, &'static str)> {
+    let html_string = fs::read_to_string("./dist/index.html").map_err(|_| {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "index file could not be found",
+        )
+    })?;
+
+    Ok(Html(html_string))
+    // Read the index file (return 500 if impossible)
+    // pull from the dist dir
+    // then on deploy copy the dist dir from Vue into the rust app
+    // Router::new().
+    // ServeFile::new("assets/index.html")
+    // Html(include_str!("dist/assets/index.html"))
 }
