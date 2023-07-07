7b21bad47e42f1bab4a99025be5697604de4965b
Tim Hoiberg <tim.hoiberg@gmail.com>
2023-07-07 23:06:17 +0900

    Further tweak CSS to reduce re-definition

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -24,7 +24,7 @@ import Anki from '@/components/AnkiSection.vue'
 
 <style scoped>
 .grid-container {
-  min-height: 100%;
+  height: 100dvh;
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-template-rows: 1fr 1fr;
@@ -32,7 +32,6 @@ import Anki from '@/components/AnkiSection.vue'
 
 @media screen and (max-width: 726px) {
   .grid-container {
-    max-height: 100dvh;
     grid-template-columns: 1fr;
     grid-template-rows: repeat(4, 100dvh);
     scroll-snap-type: y mandatory;
