34a12bbb8c01f3385dcf05867e935fcfc32a3c29
Tim Hoiberg <tim.hoiberg@gmail.com>
2023-07-28 16:17:02 +0900

    Remove old HTML code

--- backend/src/api/anki/data.rs
+++ backend/src/api/anki/data.rs
@@ -11,34 +11,6 @@ pub struct AnkiData {
     data_updated_at: DateTime<Utc>,
 }
 
-// TODO: Figure out better errors
-// I want to model specific error states for:
-//   - More/Less fields than expected
-//   - Fields not being parsable into u32
-#[derive(Debug)]
-struct MissingHTMLError;
-
-impl std::error::Error for MissingHTMLError {}
-
-impl fmt::Display for MissingHTMLError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Unable to find the right HTML")
-    }
-}
-
-impl AnkiData {
-    pub fn new(elements: Vec<String>) -> anyhow::Result<Self> {
-        let active_review_count = elements.get(0).ok_or(MissingHTMLError)?.parse::<u32>()?;
-        let new_card_count = elements.get(1).ok_or(MissingHTMLError)?.parse::<u32>()?;
-
-        Ok(Self {
-            active_review_count,
-            new_card_count,
-            data_updated_at: Utc::now(),
-        })
-    }
-}
-
 impl From<DeckInfo> for AnkiData {
     fn from(deck: DeckInfo) -> Self {
         deck.review_card_count();
@@ -48,20 +20,5 @@ impl From<DeckInfo> for AnkiData {
             new_card_count: deck.new_card_count() as u32,
             data_updated_at: Utc::now(),
         }
-    }
-}
-
-#[cfg(test)]
-mod test_super {
-    use super::*;
-
-    #[test]
-    fn test_creates_anki_data_with_valid_data() {
-        let data = vec!["10".to_owned(), "20".to_owned()];
-
-        let anki_data = AnkiData::new(data).unwrap();
-
-        assert_eq!(anki_data.active_review_count, 10);
-        assert_eq!(anki_data.new_card_count, 20);
     }
 }
