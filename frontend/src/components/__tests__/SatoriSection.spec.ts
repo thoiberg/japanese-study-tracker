0a008ed00d98595ae8830210f0f909b921bac4e7
Tim Hoiberg <tim.hoiberg@gmail.com>
2023-12-01 00:21:57 +0900

    Add Daily indicator to Satori

--- frontend/src/components/__tests__/SatoriSection.spec.ts
+++ frontend/src/components/__tests__/SatoriSection.spec.ts
@@ -1,8 +1,9 @@
 import { flushPromises, mount } from '@vue/test-utils'
 import { afterEach, describe, vi, it, expect } from 'vitest'
-import SatoriSectionVue from '../SatoriSection.vue'
+import SatoriSectionVue, { type SatoriResponse } from '../SatoriSection.vue'
 import LoadingIndicatorVue from '../LoadingIndicator.vue'
 import UpdatedTimestampVue from '../UpdatedTimestamp.vue'
+import DailyGoalIndicatorVue from '../DailyGoalIndicator.vue'
 
 describe('SatoriSection', () => {
   const fetchMock = vi.fn()
@@ -24,18 +25,26 @@ describe('SatoriSection', () => {
   })
 
   describe('when the request succeeds', () => {
-    it('displays the information', async () => {
-      const data = {
-        data_updated_at: '2023-06-24T06:00:00Z',
-        new_card_count: 20,
-        active_review_count: 8
+    interface MockSatoriResponse extends Partial<SatoriResponse> {}
+
+    function mockResponse(mockData?: MockSatoriResponse) {
+      const data: SatoriResponse = {
+        data_updated_at: mockData?.data_updated_at || '2023-06-24T06:00:00Z',
+        new_card_count: mockData?.new_card_count || 20,
+        active_review_count: mockData?.active_review_count || 8,
+        daily_study_goal_met: mockData?.daily_study_goal_met || false
       }
+
       const mockResponse = {
         status: 200,
         ok: true,
         json: () => new Promise((resolve) => resolve(data))
       }
       fetchMock.mockResolvedValue(mockResponse)
+    }
+
+    it('displays the information', async () => {
+      mockResponse()
 
       const wrapper = mount(SatoriSectionVue)
       await flushPromises()
@@ -45,6 +54,28 @@ describe('SatoriSection', () => {
       expect(wrapper.findComponent(UpdatedTimestampVue).text()).toEqual(
         'Data Fetched at: 24/6/23, 3:00 pm'
       )
+    })
+
+    describe('when the study goal has not been met', () => {
+      it('does not show the study indicator', async () => {
+        mockResponse({ daily_study_goal_met: false })
+
+        const wrapper = mount(SatoriSectionVue)
+        await flushPromises()
+
+        expect(wrapper.findComponent(DailyGoalIndicatorVue).exists()).toBe(false)
+      })
+    })
+
+    describe('when the study goal has met', () => {
+      it('shows the study indicator', async () => {
+        mockResponse({ daily_study_goal_met: true })
+
+        const wrapper = mount(SatoriSectionVue)
+        await flushPromises()
+
+        expect(wrapper.findComponent(DailyGoalIndicatorVue).exists()).toBe(true)
+      })
     })
   })
 
